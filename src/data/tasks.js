7f57f5e6ea95a36ef92601a4dba537d27c0a77ae
dedmarks <dedmark99@gmail.com>
2022-10-13 00:52:03 +0200

    task2

---
+++ src/data/tasks.js
@@ -0,0 +1,202 @@
+export const tasks = [
+    {
+        title: "Application Setup",
+        status: "completed",
+        buisness_context:[
+            [
+                {
+                    card_view: true,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: true,
+                    card_tag: "none",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "Application has been accepted 🎉 🙌",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: false,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                }
+            ],
+            {
+                title: "Application has been accepted 🎉 🙌",
+                img: "url",
+                sender_name: "Kristen Anistone",
+                date: "Today, 17th December",
+                time: "11:20",
+                message:"Hello!\n My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted!  🎉  🙌\n You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.\n Here's the project overview:You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint."
+            },
+        ]
+    },
+    {
+        title: "Static Books List",
+        status: "completed",
+        buisness_context:[
+            [
+                {
+                    card_view: true,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: true,
+                    card_tag: "none",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "Application has been accepted 🎉 🙌",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: false,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                }
+            ],
+            {
+                title: "Application has been accepted 🎉 🙌",
+                img: "url",
+                sender_name: "Kristen Anistone",
+                date: "Today, 17th December",
+                time: "11:20",
+                message:"Hello!\n My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted!  🎉  🙌\n You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.\n Here's the project overview:You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint."
+            },
+        ]
+    },
+    {
+        title: "Administartor Panel",
+        status: "completed",
+        buisness_context:[
+            [
+                {
+                    card_view: true,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: true,
+                    card_tag: "none",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "Application has been accepted 🎉 🙌",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: false,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                }
+            ],
+            {
+                title: "Application has been accepted 🎉 🙌",
+                img: "url",
+                sender_name: "Kristen Anistone",
+                date: "Today, 17th December",
+                time: "11:20",
+                message:"Hello!\n My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted!  🎉  🙌\n You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.\n Here's the project overview:You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint."
+            },
+        ]
+    },
+    {
+        title: "Connect Admin wit Frontend",
+        status: "active",
+        buisness_context:[
+            [
+                {
+                    card_view: true,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: true,
+                    card_tag: "none",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "Application has been accepted 🎉 🙌",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: false,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                }
+            ],
+            {
+                title: "Application has been accepted 🎉 🙌",
+                img: "url",
+                sender_name: "Kristen Anistone",
+                date: "Today, 17th December",
+                time: "11:20",
+                message:"Hello!\n My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted!  🎉  🙌\n You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.\n Here's the project overview:You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint."
+            },
+        ]
+    },
+    {
+        title: "Book Review Feature",
+        status: "blocked",
+        buisness_context:[
+            [
+                {
+                    card_view: true,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: true,
+                    card_tag: "none",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "Application has been accepted 🎉 🙌",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                },
+                {
+                    card_view: false,
+                    card_tag: "new",
+                    card_name: "Olga Nelson",
+                    card_date: "Dec 17",
+                    card_title: "New sprint, tasks and informations.",
+                    card_message: "Hi Eric, congratulations on completeing the previous assignment. This time you will have to focus on..."
+                }
+            ],
+            {
+                title: "Application has been accepted 🎉 🙌",
+                img: "url",
+                sender_name: "Kristen Anistone",
+                date: "Today, 17th December",
+                time: "11:20",
+                message:"Hello!\n My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted!  🎉  🙌\n You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.\n Here's the project overview:You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint."
+            },
+        ]
+    },
+]
